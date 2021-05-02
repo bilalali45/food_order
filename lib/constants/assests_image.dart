00fca7202b6a8ec4ba12fc17d949d805a8af7ebc
change <bilal.ali@technado.co>
2021-05-03 01:33:00 +0500

    commit update list

--- lib/constants/assests_image.dart
+++ lib/constants/assests_image.dart
@@ -1,5 +1,6 @@
-final String imageAssetsRoot = "images/";
-
+final String imageAssetsRoot = "Images/";
+final String image1 = _getImagePath("01.png");
+final String splash_logo = _getImagePath("thevenuelogo.png");
 
 
 String _getImagePath(String fileName){
--- lib/screens/splash/SplashScreen.dart
+++ lib/screens/splash/SplashScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_order/constants/assests_image.dart';
+import 'package:food_order/widgets/loading.dart';
 
 
 class SplashScreen extends StatelessWidget {
@@ -6,14 +8,32 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(logo, width: 120,),
-          SizedBox(height: 10,),
-          Loading()
-        ],
-      ),
+        body: new Container(
+            child: Container(
+            constraints: BoxConstraints.expand(),
+             decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(image1),
+                    //image: NetworkImage("https://flutter-examples.com/wp-content/uploads/2020/02/dice.jpg"),
+                    fit: BoxFit.cover)
+
+            ),
+            child: Center(
+              child: Image.asset(splash_logo, width: 150,),
+
+            )
+
+        )
+
+      // body: Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: [
+      //     Image.asset(splash_logo, width: 120,),
+      //     SizedBox(height: 10,),
+      //     Loading()
+      //   ],
+      // ),
+        )
     );
   }
 }
