3e88f046169c99ded648ced20c12840d0cd763bd
change <bilal.ali@technado.co>
2021-05-04 02:25:03 +0500

    commit

--- lib/screens/home/MyHomePage.dart
+++ lib/screens/home/MyHomePage.dart
@@ -10,10 +10,19 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        body: Center(
+        child: Column(
+            data()
 
+        )
+      )
     );
   }
 
 
+
+}
+
+data() {
 }
 
