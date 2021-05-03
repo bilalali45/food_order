31e5d34f1aa07ab184a388f6415bd7b97e693751
change <bilal.ali@technado.co>
2021-05-04 01:48:08 +0500

    listdata

--- lib/screens/splash/SplashScreen.dart
+++ lib/screens/splash/SplashScreen.dart
@@ -1,31 +1,49 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:food_order/constants/assests_image.dart';
+import 'package:food_order/screens/home/MyHomePage.dart';
 import 'package:food_order/widgets/loading.dart';
 
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    Timer(
+        Duration(seconds: 3),
+            () =>
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (BuildContext context) => MyHomePage())));
+
     return Scaffold(
       backgroundColor: Colors.white,
-        body: new Container(
-            child: Container(
+        body: new Stack(
+            children: <Widget>[
+        new Container(
             constraints: BoxConstraints.expand(),
-             decoration: BoxDecoration(
+               decoration: BoxDecoration(
                 image: DecorationImage(
                     image: AssetImage(image1),
                     //image: NetworkImage("https://flutter-examples.com/wp-content/uploads/2020/02/dice.jpg"),
                     fit: BoxFit.cover)
 
+
             ),
             child: Center(
-              child: Image.asset(splash_logo, width: 150,),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(splash_logo, width: 120,),
+                    SizedBox(height: 10,),
+                    Loading()
+                  ],
+              ),
 
-            )
+    )
 
-        )
 
-      // body: Column(
+    // body: Column(
       //   mainAxisAlignment: MainAxisAlignment.center,
       //   children: [
       //     Image.asset(splash_logo, width: 120,),
@@ -33,7 +51,13 @@ class SplashScreen extends StatelessWidget {
       //     Loading()
       //   ],
       // ),
-        )
+
+
+       )
+
+
+     ]
+     )
     );
   }
 }
