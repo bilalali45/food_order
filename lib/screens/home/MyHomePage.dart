de0f5e1c624fb4cd2efb245f620557d300d94f44
change <bilal.ali@technado.co>
2021-06-19 23:24:12 +0500

    changes 015

--- lib/screens/home/MyHomePage.dart
+++ lib/screens/home/MyHomePage.dart
@@ -20,6 +20,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  final GlobalKey<ScaffoldState> _scaffoldState = GlobalKey();
 
   int currentTab = 0;
   final List<Widget> screens = [
@@ -35,9 +36,96 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Food order'),
-        ),
+      key: _scaffoldState,
+
+      appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: (){
+          _scaffoldState.currentState.openDrawer();
+        }),
+
+        title: Text('Page title'),
+        actions: [
+          Icon(Icons.favorite),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Icon(Icons.search),
+          ),
+          Icon(Icons.more_vert),
+        ],
+        backgroundColor: Colors.purple,
+      ),
+
+
+      drawer: Drawer(child: ListView(
+        children: <Widget>[
+          Container(
+            child: UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"),
+              ),
+              accountName: Text(
+                "John Paul",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              accountEmail: Text(
+                "johnpaul@example.com",
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text('My Files'),
+            leading: Icon(Icons.folder),
+            onTap: () {
+              print("Clicked");
+            },
+          ),
+          ListTile(
+            title: Text('Shared with me'),
+            leading: Icon(Icons.people),
+            onTap: () {
+              print("Clicked");
+            },
+          ),
+          ListTile(
+            title: Text('Starred'),
+            leading: Icon(Icons.star),
+            onTap: () {
+              print("Clicked");
+            },
+          ),
+          ListTile(
+            title: Text('Recent'),
+            leading: Icon(Icons.timer),
+            onTap: () {
+              print("Clicked");
+            },
+          ),
+          ListTile(
+            title: Text('Offline'),
+            leading: Icon(Icons.offline_pin),
+            onTap: () {
+              print("Clicked");
+            },
+          ),
+          ListTile(
+            title: Text('Uploads'),
+            leading: Icon(Icons.file_upload),
+            onTap: () {
+              print("Clicked");
+            },
+          ),
+          ListTile(
+            title: Text('Backups'),
+            leading: Icon(Icons.backup),
+            onTap: () {
+              print("Clicked");
+            },
+          ),
+        ],
+      ),
+      ),
 
       //  body: Center(child: Column(children: [data(context)])));
 
